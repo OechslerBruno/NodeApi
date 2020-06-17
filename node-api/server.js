42fa3c279cca2a57f2cf7d32da5ea8464a759981
OechslerBruno <oechsler.bruno33@gmail.com>
2020-06-17 15:19:52 -0300

    [FEAT] First route
    First route using express.

---
+++ node-api/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+//Every time when user acces the root route
+//req is the resition to the server.
+//res is the response for the resition.
+app.get('/', (req, res) => {
+    res.send('Hello Bruno!');
+}); 
+
+app.listen(3001); //Tell the app to listen the port 3001
